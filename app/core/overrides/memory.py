f12f0b0efa34695b52e58cb17e036921a317ddce
I-Hsien <32727642+iridium-soda@users.noreply.github.com>
2025-01-20 11:53:20 +0800

    chore:sync

--- app/core/overrides/memory.py
+++ app/core/overrides/memory.py
@@ -7,11 +7,11 @@ from typing import DefaultDict
 from metagpt.memory import Memory
 from metagpt.schema import Message
 from pydantic import Field, SerializeAsAny
-
+from schema import MessageWithLabel
 
 class MemoryWithLabel(Memory):
-    index: DefaultDict[str, list[SerializeAsAny[Message]]] = Field(default_factory=lambda: defaultdict(list))
+    index: DefaultDict[str, list[SerializeAsAny[MessageWithLabel]]] = Field(default_factory=lambda: defaultdict(list))
 
-    def get_by_label(self, label: str) -> list[Message]:
+    def get_by_label(self, label: str) -> list[MessageWithLabel]:
         """Return all messages with a specified label"""
         return [message for message in self.storage if message.label == label]
